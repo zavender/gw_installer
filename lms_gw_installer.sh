e5b0389ed8473de98b2c2b6526e4239aee3b3dde
zavender <patrik.humeny@gmail.com>
2020-07-30 20:22:57 +0200

    Update lms_gw_installer.sh

--- lms_gw_installer.sh
+++ lms_gw_installer.sh
@@ -52,11 +52,61 @@ PKG_FILE="packages.conf"
 
 ### PRECHECKS
 
-source ${BIN_PATH}/${PRECK_SCRPT}
-if [ $? != 0 ];  then
-	echo [FAILED]	Prechecks failed
-	exit 1
+### PRECHECKS
+# Checking for supported package
+PKG_NAME=`ls -1 $PKG_PATH | grep -wf ${CONF_PATH}/${PKG_FILE} | grep -v "#" | tail -1`
+PKG_VER=`echo $PKG_NAME | awk -F"-" {'print $2"-"$3"-"$4"-"$5$6'}`
+if [ ! -z $PKG_NAME ]; then
+	echo "Package version $PKG_VER found"
+	echo "[OK]	Supported package found"
+else
+	echo "[FAILED]	Supported Package found"
+	echo "NO supported package found at path $PKG_PATH"
+	exit 20
 fi
+
+# Checking execute permissions for $PKG_NAME
+if [ ! -x ${PKG_PATH}/${PKG_NAME} ]; then
+        echo "Package $PKG_NAME cannot be executed"
+        echo "Current permissions"
+        ls -l ${PKG_PATH}/${PKG_NAME}
+        echo "Changing permissions"
+        chmod u+x  ${PKG_PATH}/${PKG_NAME}
+        ls -l ${PKG_PATH}/${PKG_NAME}
+fi
+if [ ! -x ${PKG_PATH}/${PKG_NAME} ]; then
+                echo "[FAILED]	Execute permission"
+                exit 21
+else
+        echo "[OK]	Execute permissions"
+fi
+
+# Looking for configuration file
+if [ ! -f ${CONF_PATH}/$CONF_FILE ];then
+	echo "[FAILED]	Config file found"
+        echo "$CONF_FILE NOT FOUND! at path $CONF_PATH"
+        exit 22
+fi
+echo "[OK]	Config file found"
+
+# Check whether hostname -f is pingable
+ping -c 1 $HOSTNAME
+if [ $? = 0 ]; then	
+	echo [OK] server $HOSTNAME is pingable!
+else	
+	echo "Please add primary IP and HOSTNAME into /etc/hosts"
+	exit  25
+fi
+
+#FINAL STATUS
+echo [OK]	ALL prechecks succesfully completed
+#exit 0
+
+#source ${BIN_PATH}/${PRECK_SCRPT}
+#if [ $? != 0 ];  then
+#	echo [FAILED]	Prechecks failed
+#	exit 1
+#fi
 	
 
 # Check whether GW is already installed
